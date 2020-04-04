fddffaa81a62d55109493cd7434ac28b4cc5f56b
areut-su <areut.su@gmail.com>
2020-04-04 13:40:33 +0700

    init

---
+++ product_cat_tree/product_cat_tree.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *  use [PRODUCT_CAT_TREE level=1]
+ *  or [PRODUCT_CAT_TREE]
+ *      default level=1
+ */
+
+add_shortcode('PRODUCT_CAT_TREE', 'product_cat_tree_print');
+
+/**
+ *
+ * @param array $atts
+ *   product_cat_tree_print(['level'=>1])
+ */
+function product_cat_tree_print($atts)
+{
+    if (!class_exists('CatTree'))
+    {
+        include(__DIR__ . DIRECTORY_SEPARATOR . 'CatTree.php');
+    }
+    $level = isset($atts['level']) ? $atts['level'] : 1;
+
+    try
+    {
+        $m = CatTree::create(get_categories(['taxonomy' => 'product_cat',]), $level);
+    }
+    catch (Exception $e)
+    {
+        echo '<pre>';
+        echo $e->getMessage();
+        echo '</pre>';
+    }
+    echo implode($m->makeTree());
+}
+
---
+++ product_cat_tree/test_product_cat_tree.php
@@ -0,0 +1,27 @@
+<?php
+
+function urlTest($id)
+{
+    return $id;
+}
+
+function test_product_cat_tree()
+{
+    // create test file  with JSON_UNESCAPED_UNICODE
+    // json_encode($allCat, JSON_UNESCAPED_UNICODE);
+    $fileCat = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'test_cats.json');
+    $fileCat = json_decode($fileCat);
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'CatTree.php');
+    try
+    {
+        $m = CatTree::create($fileCat, 1, 'urlTest');
+    }
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+    }
+    print_r($m->makeTree());
+}
+
+test_product_cat_tree();
+
