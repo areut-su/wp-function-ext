4462d485502116b5838a9eee0ab10eb7769b31df
areut-su <areut.su@gmail.com>
2020-04-04 13:50:26 +0700

    уточнено описание

--- product_cat_tree/product_cat_tree.php
+++ product_cat_tree/product_cat_tree.php
@@ -10,10 +10,11 @@ add_shortcode('PRODUCT_CAT_TREE', 'product_cat_tree_print');
 /**
  *
  * @param array $atts
- *   product_cat_tree_print(['level'=>1])
+ *   product_cat_tree_print(['level'=>1]);
  */
 function product_cat_tree_print($atts)
 {
+
     if (!class_exists('CatTree'))
     {
         include(__DIR__ . DIRECTORY_SEPARATOR . 'CatTree.php');
